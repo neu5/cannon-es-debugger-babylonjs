4d56aae363fa7e7668a85ce01ee739d1ca21e5e4
neu5 <neus90@gmail.com>
2022-10-09 21:30:58 +0200

    Replace Rollup with Parcel as building tool #12

--- dist/cannon-es-debugger.d.ts
+++ dist/cannon-es-debugger.d.ts
@@ -1,14 +1,13 @@
-declare module "cannon-es-debugger" {
-    import { Shape } from 'cannon-es';
-    import { Color3, Mesh, Scene } from '@babylonjs/core';
-    import type { Body, World } from 'cannon-es';
-    export type DebugOptions = {
-        color?: string | number | Color3;
-        scale?: number;
-        onInit?: (body: Body, mesh: Mesh, shape: Shape) => void;
-        onUpdate?: (body: Body, mesh: Mesh, shape: Shape) => void;
-    };
-    export default function CannonDebugger(scene: Scene, world: World, { color, scale, onInit, onUpdate }?: DebugOptions): {
-        update: () => void;
-    };
-}
+import { Shape, Body, World } from "cannon-es";
+import { Color3, Mesh, Scene } from "@babylonjs/core";
+export type DebugOptions = {
+    color?: string | number | Color3;
+    scale?: number;
+    onInit?: (body: Body, mesh: Mesh, shape: Shape) => void;
+    onUpdate?: (body: Body, mesh: Mesh, shape: Shape) => void;
+};
+export default function CannonDebugger(scene: Scene, world: World, { color, scale, onInit, onUpdate }?: DebugOptions): {
+    update: () => void;
+};
+
+//# sourceMappingURL=cannon-es-debugger.d.ts.map
