fa568f492ef84f192b7e14177cd94aaa1109512e
neu5 <neus90@gmail.com>
2022-10-19 13:52:51 +0200

    Align the output target with the BabylonJS (ES2018) #22 (#23)

---
+++ rollup.config.test.prod.js
@@ -0,0 +1,44 @@
+import babel from "@rollup/plugin-babel";
+import resolve from "@rollup/plugin-node-resolve";
+
+const extensions = [".ts"];
+
+const babelOptions = {
+  babelrc: false,
+  extensions,
+  exclude: "**/node_modules/**",
+  babelHelpers: "bundled",
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        loose: true,
+        modules: false,
+        targets: ">0.5%, not dead, not ie 11, not op_mini all",
+      },
+    ],
+    "@babel/preset-typescript",
+  ],
+};
+
+export default [
+  {
+    input: `./src/cannon-es-debugger`,
+    output: {
+      file: `dist/cannon-es-debugger.js`,
+      format: "esm",
+    },
+    plugins: [resolve({ extensions }), babel(babelOptions)],
+    external: ["cannon-es", "../node_modules/@babylonjs/core/index.js"],
+  },
+  // {
+  //   input: `./src/cannon-es-debugger`,
+  //   output: {
+  //     file: `dist/cannon-es-debugger.cjs.js`,
+  //     format: "cjs",
+  //     exports: "default",
+  //   },
+  //   plugins: [resolve({ extensions }), babel(babelOptions)],
+  //   external: ["cannon-es", "@babylonjs/core"],
+  // },
+];
